6ac2c0bf9317728d377c129f0bed4a65ca5f7a02
Luis Perez <Luis_Perez64@live.com>
2016-04-05 13:38:57 -0400

    Changed Directory structure

---
+++ home.php
@@ -0,0 +1,42 @@
+<!--
+You may use this in any way, on any site, all we ask is that you please keep the link(s) crediting the author.   Though this is not mandatory, it is certainly appreciated.
+ -->
+ <!--
+Not sure if this is frowned upon in some manner or another. Using a template site for the Asthetics of the project. All the work in the Back ENd is done in PHP, and a little bit of JS
+-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<title>Administrative Operations HomePage</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<link rel="stylesheet" type="text/css" href="bluebliss.css" />
+</head>
+<body>
+<div id="mainContentArea">
+	<div id="contentBox">
+        <div id="title">Admin Operations</div>
+        
+        <div id="linkGroup">
+            <div class="link"><a href="home.php">Home</a></div>
+            <div class="link"><a href="studentOperations.php">Student Operations</a></div>
+            <div class="link"><a href="graduationList.php">Grads List</a></div>
+            <div class="link"><a href="about.php">Lol</a></div>
+        </div>
+        
+        <div id="blueBox"> 
+          <div id="header"></div>
+          <div class="contentTitle">Administrative Points</div>
+            <div class="pageContent">
+              <p>The Purpose of This Project is more or less to Display knowledge of Base Concepts in the realm of HTML/PHP with relation to SQL Queries.</p>
+              <p>For List of Student Operations (Add Student, Enroll Student, Change Grades...) Please Click the student Operations Tab.</p>
+              <p>To See a Lis of Students Eligible to Graduate. Click the List Of Students Graduating Tab. Single students progress can be assessed in Student Operations.
+              </p>
+              <!-- The ABout Us Tab is probably going to be removed. IDK what to place there-->
+              <p>The About Us Tab Highlights some of the points putforward by the team.</p>
+            </div>
+            <div id="footer"><a href="http://www.aszx.net">Template Developed</a> by <a href="http://www.bryantsmith.com">bryant smith</a></div>
+        </div>
+	</div>
+</div>
+</body>
+</html>
