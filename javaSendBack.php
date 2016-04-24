095d688312a657b40eefdae5d50c34faf695ac35
Luis Perez <Luis_Perez64@live.com>
2016-04-23 20:16:10 -0400

    Example log file.

--- javaSendBack.php
+++ javaSendBack.php
@@ -1,12 +1,12 @@
 <?php
 require_once './DBFunctions.php';
 function _begin() {
-	if(!array_key_exists('SID#', $_POST)){ // Assume that they key always exists from Java Side
+	if(!array_key_exists('SID', $_POST)){ // Assume that they key always exists from Java Side
 	//return False; // Do something here, but shouldn't have to do much in the realm of what is actually happening.
 	}
 $return = "";
 // Test if the person exists in the DB
-$sid = '1010109';//$_POST['SID#'];
+$sid = '1010109';//$_POST['SID'];
 $query = "Select * FROM students where SID = $sid";
 $result = QueryDB($query, 3);
 if(!mysqli_num_rows($result)){ // This person does not exists, handle that accordingly
@@ -20,14 +20,14 @@ if(!mysqli_num_rows($result)){ // This person does not exists, handle that accor
 	$advisor = mysqli_fetch_array(QueryDB($query,3))['name'];
 	$ret = eligibleTograduate($sid);
 	$return .= "{
-		<br>'grad': $ret[canThey],
-		<br>'Name': $name,
-		<br>'StudentID': $SID,
-		<br>'GPA': $ret[GPA],
-		<br>'Advisor': $advisor,
-		<br>'Major': $major,
-		<br>'career': $career,
-		<br>'degreeHeld': $degreeHeld,
+		<br>'grad': '$ret[canThey]',
+		<br>'Name': '$name',
+		<br>'StudentID': '$SID',
+		<br>'GPA': '$ret[GPA]',
+		<br>'Advisor': '$advisor',
+		<br>'Major': '$major',
+		<br>'career': '$career',
+		<br>'degreeHeld': '$degreeHeld',
 		<br>'courseList':["; // Append the course list here.
 	$return .= _gradeData($sid)."<br>]}";
 	} // Maybe integrate some JS to get things rolling if need be.
@@ -52,7 +52,7 @@ function _gradeData($sid) {
 		$q = $row['cName']; $w = $row['grade']; $e = $row['yearID'];
 		$r = $row['semID']; $t = $row['secID'];
 		$out.="<br>";
-		$out .= "{courseName: $q , grade: $w, yearTaken: $e, semester: $r, section: $t},";
+		$out .= "{'courseName': '$q' , 'grade': '$w', 'yearTaken': '$e', 'semester': '$r', 'section': '$t'},";
 	}
 	$out = substr($out, 0, -1); // Remove that last ,
 	return $out;
